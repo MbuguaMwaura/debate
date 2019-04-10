a7e048c910318a6780a29cc3437bfbdbe7cc93c1
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-04-10 07:06:43 +0300

    creates an authentication page to test out the firebase authentication system

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,109 @@
- // Initialize Firebase
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCKUWhdRIDgK4uM412c2x5MIFTOjsEqg8",
+    authDomain: "authentication-c8f7f.firebaseapp.com",
+    databaseURL: "https://authentication-c8f7f.firebaseio.com",
+    projectId: "authentication-c8f7f",
+    storageBucket: "",
+    messagingSenderId: "315873449998"
+  };
+  firebase.initializeApp(config);
+
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnLogout = document.getElementById('btnLogout');
+ 
+  btnLogin.addEventListener('click', e => {
+      const email = txtEmail.value;
+      const pass = txtPassword.value;
+      const auth = firebase.auth();
+
+      const promise = auth.signInWithEmailAndPassword(email,pass);
+      promise.catch(e => console.log(e.message));
+  });
+
+  btnSignUp.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email,pass);
+    promise.catch(e => console.log(e.message));
+  });
+  
+  btnLogout.addEventListener('click', e => {
+      firebase.auth().signOut();
+  });
+
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(firebaseUser){
+          console.log(firebaseUser);
+          btnLogout.classList.remove('hide');
+      } else {
+          console.log('not logged in' );
+          btnLogout.classList.add('hide');
+      }
+  });
+}());
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /* Initialize Firebase
  var config = {
     apiKey: "AIzaSyD0TgvYXkEeiAAJfjaLmm-lS2OerJpFZ1I",
     authDomain: "debate-ad49b.firebaseapp.com",
@@ -8,8 +113,6 @@
     messagingSenderId: "270217448754"
   };
   firebase.initializeApp(config);
- 
- 
   var firestore = firebase.firestore();
   
   const docRef = firestore.doc("samples/sandwichData");
@@ -51,4 +154,4 @@ getRealtimeUpdates = function(){
     })
 }
 
-getRealtimeUpdates();
+getRealtimeUpdates();*/
