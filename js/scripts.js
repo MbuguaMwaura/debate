124a358b6b8ff2753682487bfbdb9e3daa916cbd
Christine <you@example.com>
2019-04-10 17:47:25 +0300

    added functioniality to the comment button in landingpage.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,44 @@
 $(document).ready(function () {
     document.getElementById("btnTopic").addEventListener("click", function () {
         var text = topic.value;
-        var paragraph = document.createElement("p");
+        var paragraph = document.createElement("li");
         var node = document.createTextNode(text);
         paragraph.appendChild(node);
         var element = document.getElementById("submitted");
         element.appendChild(paragraph);
-        //document.getElementById("top").innerHTML += '<p>' + text
-        //document.getElementById("topic").value = ""
-
+       
         $("#btnAdd").show();
         $("#formTopic").hide();
+        $("li").addClass('something'); 
 
-        document.getElementById("btnAdd").addEventListener("click", function () {
-            $("#btnAdd").hide();
-            $("#formTopic").show();
-        })
+         document.getElementById("btnAdd").addEventListener("click", function () {
+             $("#btnAdd").hide();
+             $("#formTopic").show();
+         })
+
+        
+       var button = document.createElement("button");
+       button.innerHTML = "comment";
+
+        button.addEventListener("click", function () {
+             $("#formTopic").show();
+        });
 
 
-        function getwords() {
-            text = words.value;
-            document.getElementById("para").innerHTML += '<p>' + text
-            document.getElementById("words").value = ""
-        }
+       
+       var i;
+        for (i = 0; ;i++) {
+       var body = document.getElementsByClassName("something")[i];
+       body.appendChild(button);
+      
+        };
+     
+       
+        
 
-        function fetchwords() {
-            text = word.value;
-            document.getElementById("p").innerHTML += '<p>' + text
-            document.getElementById("word").value = ""
-        }
+       
+    
+   
+        
     });
 });
