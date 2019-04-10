accf6a1e8fc3e89408f2c33a4393cd61c4c74a37
Christine <you@example.com>
2019-04-10 14:25:00 +0300

    added functions to the buttons in landingpage.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
-function gettopic() {
-    text = topic.value;
-    document.getElementById("top").innerHTML += '<p>' + text
-    document.getElementById("topic").value = "Write your debate topic here"
-}
+$(document).ready(function () {
+    document.getElementById("btnTopic").addEventListener("click", function () {
+        var text = topic.value;
+        document.getElementById("top").innerHTML += '<p>' + text
+        //document.getElementById("topic").value = ""
+
+        $("#btnAdd").show();
+        $("#formTopic").hide();
+
+        document.getElementById("btnAdd").addEventListener("click", function () {
+            $("#btnAdd").hide();
+            $("#formTopic").show();
+        })
 
 
-function getwords() {
-    text = words.value;
-    document.getElementById("para").innerHTML += '<p>' + text
-    document.getElementById("words").value = ""
-}
+        function getwords() {
+            text = words.value;
+            document.getElementById("para").innerHTML += '<p>' + text
+            document.getElementById("words").value = ""
+        }
 
-function fetchwords() {
-    text = word.value;
-    document.getElementById("p").innerHTML += '<p>' + text
-    document.getElementById("word").value = ""
-}
+        function fetchwords() {
+            text = word.value;
+            document.getElementById("p").innerHTML += '<p>' + text
+            document.getElementById("word").value = ""
+        }
+    });
+});
